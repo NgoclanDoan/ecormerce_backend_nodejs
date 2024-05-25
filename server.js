bcc337ee178f84f24b28d108e31ab47a08e8a415
Đoàn Thị Ngọc Lan <lan.doan@didongviet.vn>
2024-05-25 23:19:57 +0700

    S2: Những folder và packages cần thiết khi khởi tạo Project

---
+++ server.js
@@ -0,0 +1,15 @@
+const app = require("./src/app");
+
+// Khai báo port
+const PORT = 3055;
+
+// Khởi động network NodeJS
+const server = app.listen(PORT, () => {
+  console.log(`Server eCommerce is running on port ${PORT}`);
+});
+
+// process.on("SIGINT", () => {
+//   server.close(() => {
+//     console.log("Exit Server Express");
+//   });
+// });
