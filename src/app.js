5a63a416b8573035c7d51a4637ad4956d540a947
Đoàn Thị Ngọc Lan <lan.doan@didongviet.vn>
2024-05-25 23:19:57 +0700

    S2: Những folder và packages cần thiết khi khởi tạo Project

---
+++ src/app.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const app = express();
+const morgan = require("morgan");
+const helmet = require("helmet");
+const compression = require("compression");
+
+// ========= init middleware =========
+
+/**
+ * morgan: format log các request
+ */
+app.use(morgan("dev")); // Sử dụng ở development
+// app.use(morgan("combined")); // Theo tiêu chuẩn apache. Nên sử dụg ở production
+// app.use(morgan("common"));
+// app.use(morgan("short"));
+// app.use(morgan("tiny"));
+
+/**
+ * helmet: bảo vệ thông tin riêng tư, chăn chặn trang thứ 3 truy cập vào đọc cookie
+ */
+app.use(helmet());
+
+/**
+ * compress: nén dữ liệu trước khi gửi đi, giúp giảm dung lượng dữ liệu
+ */
+app.use(compression());
+
+// ========= init db =========
+
+// ========= init routers =========
+app.get("/", (req, res, next) => {
+  const strCompress = "Hello World";
+  return res.status(200).json({
+    message: "Hello World",
+    metadata: strCompress.repeat(1000000),
+  });
+});
+
+// ========= handle errors =========
+
+module.exports = app;
